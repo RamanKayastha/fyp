02977684a053dc7436843a6d4f899a8dee82ee3a
Raman <kayastharaman967@gmail.com>
2026-06-02 13:01:42 +0545

    this is the test commit

--- frontend/src/pages/marketing/Placeorder.jsx
+++ frontend/src/pages/marketing/Placeorder.jsx
@@ -6,7 +6,7 @@ const Placeorder = () => {
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       <div className='flex flex-col gap-4 w-full sm:w-120'>
         <div className='text-xl sm:text-2xl my-3'>
-          <Title text1={'DELIVERY'} text2={'INFORMATION'} />
+          <Title text1={'DELIVERY'} text2={'INFO'} />
         </div>
         <div className='flex  gap-3'>
         </div>
